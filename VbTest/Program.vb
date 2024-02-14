b106b4b57728a6dcc6f97b3ec3d9189c0e8d4836
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-02-14 14:58:24 +0800

    test VB

---
+++ VbTest/Program.vb
@@ -0,0 +1,12 @@
+﻿Imports ShellLibrary
+
+Module Program
+
+    Sub Main()
+        ' 调用 RunCommand 方法运行命令
+        Shell.RunCommand("ping 127.0.0.1", count:="3", hideWindow:="false", async:=False, useDataflow:=False, showProgress:=False)
+
+        Console.ReadLine()
+    End Sub
+
+End Module
